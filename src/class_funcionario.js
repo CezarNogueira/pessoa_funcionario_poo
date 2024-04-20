7f4dfe9ca779c3fb4b3aeb0a97f06b0a0970c48f
Cezar Nogueira <cezartv1221@gmail.com>
2024-04-19 23:08:59 -0300

    Criar Classe Dependente e Adicionar metodo atualizarDados()

--- src/class_funcionario.js
+++ src/class_funcionario.js
@@ -1,18 +1,30 @@
 import Pessoa from "./class_pessoa.js";
 
 class Funcionario extends Pessoa {
-    constructor(nome, idade, cargo) {
-        super(nome, idade);
+    constructor(nome, idade, cpf, cargo, registro) {
+        super(nome, idade, cpf);
         this.cargo = cargo;
+        this.registro = registro;
     }
 
-    mostrarDados() {
-        //super.mostrarDados();
-        console.log(`Sou um ${this.cargo}.`);
+    mostrarDadosFunc() {
+        super.mostrarDados();
+        console.log("---> Funcionario");
+        console.log(`Cargo: ${this.cargo}`);
+        console.log(`Registro: ${this.registro}`);
+    }
+
+    atualizarDados(novoNome, novaIdade, novoCpf, novoCargo, novoRegistro) {
+        super.atualizarDados(novoNome, novaIdade, novoCpf);
+        this.cargo = novoCargo;
+        this.registro = novoRegistro;
     }
 }
 
-    const Funcionario1 = new Funcionario("Cezar", 20, "Administrador");
-    Funcionario1.mostrarDados();
+    const Funcionario1 = new Funcionario("Cezar", 20, "909.694.219-06", "Estagiario", "00001");
+    Funcionario1.mostrarDadosFunc();
+
+    Funcionario1.atualizarDados("Cezar Nogueira", 21, "909.694.219-06", "Programador", "00001");
+    Funcionario1.mostrarDadosFunc();
 
     export default Funcionario;
--- src/class_gerente.js
+++ src/class_gerente.js
@@ -1,17 +1,19 @@
 import Funcionario from "./class_funcionario.js";
 
 class Gerente extends Funcionario {
-    constructor (nome, idade, cargo, setor) {
-        super(nome, idade, cargo);
+    constructor (nome, idade, cpf, cargo, setor) {
+        super(nome, idade, cpf, cargo);
         this.setor = setor;
     }
 
-    mostrarDados() {
-        console.log(`Sou Gerente do setor ${this.setor}.`);
+    mostrarDadosGer() {
+        console.log("---> Gerente");
+        super.mostrarDados();
+        console.log(`Setor: ${this.setor}`);
     }
 }
 
-const Gerente1 = new Gerente("Cezar", 20, "Administrador", "Administrativo");
-Gerente1.mostrarDados();
+const Gerente1 = new Gerente("Cezar", 20, "909.694.219-06", "Administrador", "Administrativo");
+Gerente1.mostrarDadosGer();
 
 export default Gerente;
